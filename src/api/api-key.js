b0474c0fc265cb7b1dfe7ba262f76770b57914ca
Emílio de Aguiar <85713266+emiliodaf@users.noreply.github.com>
2022-11-03 16:11:08 -0300

    adding gitignore

---
+++ src/api/api-key.js
@@ -0,0 +1,8 @@
+
+
+export const API_URL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=4f790ac4d97736336cc69ca0f10e2d4a&page=1"
+
+export const API_IMG = "https://image.tmdb.org/t/p/w1280"
+
+export const API_SEARCH = "https://api.themoviedb.org/3/search/movie?&api_key=4f790ac4d97736336cc69ca0f10e2d4a&query="
+
